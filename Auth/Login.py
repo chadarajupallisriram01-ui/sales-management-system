385b9b2ad86bb1b0d102db63352fa158deedc2fe
chadarajupallisriram01-ui <chadarajupalli.sriram01@gmail.com>
2026-03-15 14:25:17 +0530

    Refactor Login function and initialize session state

--- Auth/Login.py
+++ Auth/Login.py
@@ -11,10 +11,10 @@ st.set_page_config(layout="wide")
 
 if "emp" not in st.session_state:
     st.session_state.emp = None
-
+def init_session():
 if "User_name" not in st.session_state:
-    st.session_state.User_name = "Guest" or "" or " "
-
+    st.session_state.User_name = ""
+init_session()
 def Login():
     st.title("Login")
     with st.form("Login"):
